ebc2e45ebcba62fc944cd7ecd6740720c605f137
nicochnz <chiche.nicolas@gmail.com>
2024-10-14 16:39:14 +0200

    add home button, fix some bug, voices off when sound off

--- script.js
+++ script.js
@@ -58,109 +58,109 @@ const questions = [
       "Bordeaux présente plus de 3 000 mascarons qui participent à l'ornementation des façades et des fontaines de la ville. ",
     correct: 0,
   },
-  //   {
-  //     question:
-  //       "Qu’était le stade Chaban-Delmas avant d’être dédié au foot et au rugby ?",
-  //     answers: [
-  //       "Un pré où broutaient des chèvres",
-  //       "Un vélodrome",
-  //       "Un amphithéâtre romain",
-  //       " Un parking pour carrosses",
-  //     ],
-  //     description:
-  //       "Né en 1924, avant le stade Chaban, le premier parc des sports Lescure fut imaginé par un certain Robert Hüe, pour accueillir un grand vélodrome. ",
-  //     correct: 1,
-  //   },
-  //   {
-  //     question: "Quand est apparu le premier tramway à Bordeaux ?",
-  //     answers: ["1932", "2003", "2000", "1880"],
-  //     description:
-  //       "Lors de sa mise en circulation le 4 mai 1880, la première ligne bordelaise présentait des tramways tractés par des chevaux.",
-  //     correct: 3,
-  //   },
-  //   {
-  //     question: "Qu’est donc le Palais Rohan ?",
-  //     answers: [
-  //       "Un château dans Le Seigneur des Anneaux",
-  //       "L’actuelle mairie de Bordeaux.",
-  //       "Les bâtiments composant la Place de la Bourse.",
-  //       "Une échoppe célèbre pour ses huîtres.",
-  //     ],
-  //     description:
-  //       "Palais impérial de Napoléon Ier en 1808 et palais royal en 1815 sous Louis XVIII, le palais Rohan devient hôtel de ville en 1835. ",
-  //     correct: 1,
-  //   },
-  //   {
-  //     question: "Qu’est donc le bordeluche ?",
-  //     answers: [
-  //       "Un sac à vin bordelais.",
-  //       "Une danse traditionnelle bordelaise",
-  //       "Le vieux parlé bordelais. ",
-  //       "Une sorte de fromage.",
-  //     ],
-  //     description:
-  //       "Le bordeluche est un registre de langue autrefois très usité à Bordeaux. Il a perdu un peu de terrain, mais beaucoup de Bordelais emploient des mots spécifiques à leur région, sans le savoir. Ca daille gavé !",
-  //     correct: 2,
-  //   },
-  //   {
-  //     question: "D'où vient le nom de Bordeaux ?",
-  //     answers: [
-  //       "De l’ancien nom latin Burdigala",
-  //       "D’une légende sur les eaux du port",
-  //       "Des bordures du fleuve",
-  //       "D’une famille noble locale.",
-  //     ],
-  //     description:
-  //       "« Burdigala » serait composé de deux racines aquitaniques burd- qui signifie « boueux » et de cal- qui voudrait dire « abri, crique » et signifierait à peu près « abri dans les marais » en (proto-basque). Ce nom a évolué en Bordigala, puis Bordale en basque, Bordèu en gascon et « Bordeaux » en français.",
-  //     correct: 0,
-  //   },
-  //   {
-  //     question: "Combien de ponts y a-t-il sur l’agglomération bordelaise ?",
-  //     answers: ["3", "4", "5", "6"],
-  //     description:
-  //       "Bordeaux métropole possède huit ponts qui traversent la Garonne ; du nord au sud : le pont d'Aquitaine, le pont Jacques-Chaban-Delmas, le pont de pierre, le pont Saint-Jean, le pont ferroviaire, la passerelle Eiffel (désaffectée), le pont Simone-Veil et le pont François-Mitterrand.",
-  //     correct: 2,
-  //   },
-  //   {
-  //     question: "De quelle couleur est la Garonne sur son tronçon bordelais ?",
-  //     answers: ["Marron", "Beige", "Blonde", "Juste gavé sale"],
-  //     description:
-  //       "Blonde réaction appelée floculation entraine alors l'agglomération des particules d'argile que les courants de marée remettent en suspension.",
-  //     correct: 2,
-  //   },
-  //   {
-  //     question: "En quelle année l’Union Bordeaux Bègles a-t-elle été créée ?",
-  //     answers: ["1969", "1991", "2007", "2006"],
-  //     description:
-  //       "L'Union Bordeaux Bègles né de la fusion en 2006 du Stade bordelais université club et du Club athlétique béglais. Il est basé à Bègles dans la banlieue sud de Bordeaux en Gironde.",
-  //     correct: 3,
-  //   },
+  {
+    question:
+      "Qu’était le stade Chaban-Delmas avant d’être dédié au foot et au rugby ?",
+    answers: [
+      "Un pré où broutaient des chèvres",
+      "Un vélodrome",
+      "Un amphithéâtre romain",
+      " Un parking pour carrosses",
+    ],
+    description:
+      "Né en 1924, avant le stade Chaban, le premier parc des sports Lescure fut imaginé par un certain Robert Hüe, pour accueillir un grand vélodrome. ",
+    correct: 1,
+  },
+  {
+    question: "Quand est apparu le premier tramway à Bordeaux ?",
+    answers: ["1932", "2003", "2000", "1880"],
+    description:
+      "Lors de sa mise en circulation le 4 mai 1880, la première ligne bordelaise présentait des tramways tractés par des chevaux.",
+    correct: 3,
+  },
+  {
+    question: "Qu’est donc le Palais Rohan ?",
+    answers: [
+      "Un château dans Le Seigneur des Anneaux",
+      "L’actuelle mairie de Bordeaux.",
+      "Les bâtiments composant la Place de la Bourse.",
+      "Une échoppe célèbre pour ses huîtres.",
+    ],
+    description:
+      "Palais impérial de Napoléon Ier en 1808 et palais royal en 1815 sous Louis XVIII, le palais Rohan devient hôtel de ville en 1835. ",
+    correct: 1,
+  },
+  {
+    question: "Qu’est donc le bordeluche ?",
+    answers: [
+      "Un sac à vin bordelais.",
+      "Une danse traditionnelle bordelaise",
+      "Le vieux parlé bordelais. ",
+      "Une sorte de fromage.",
+    ],
+    description:
+      "Le bordeluche est un registre de langue autrefois très usité à Bordeaux. Il a perdu un peu de terrain, mais beaucoup de Bordelais emploient des mots spécifiques à leur région, sans le savoir. Ca daille gavé !",
+    correct: 2,
+  },
+  {
+    question: "D'où vient le nom de Bordeaux ?",
+    answers: [
+      "De l’ancien nom latin Burdigala",
+      "D’une légende sur les eaux du port",
+      "Des bordures du fleuve",
+      "D’une famille noble locale.",
+    ],
+    description:
+      "« Burdigala » serait composé de deux racines aquitaniques burd- qui signifie « boueux » et de cal- qui voudrait dire « abri, crique » et signifierait à peu près « abri dans les marais » en (proto-basque). Ce nom a évolué en Bordigala, puis Bordale en basque, Bordèu en gascon et « Bordeaux » en français.",
+    correct: 0,
+  },
+  {
+    question: "Combien de ponts y a-t-il sur l’agglomération bordelaise ?",
+    answers: ["3", "4", "5", "6"],
+    description:
+      "Bordeaux métropole possède huit ponts qui traversent la Garonne ; du nord au sud : le pont d'Aquitaine, le pont Jacques-Chaban-Delmas, le pont de pierre, le pont Saint-Jean, le pont ferroviaire, la passerelle Eiffel (désaffectée), le pont Simone-Veil et le pont François-Mitterrand.",
+    correct: 2,
+  },
+  {
+    question: "De quelle couleur est la Garonne sur son tronçon bordelais ?",
+    answers: ["Marron", "Beige", "Blonde", "Juste gavé sale"],
+    description:
+      "Blonde réaction appelée floculation entraine alors l'agglomération des particules d'argile que les courants de marée remettent en suspension.",
+    correct: 2,
+  },
+  {
+    question: "En quelle année l’Union Bordeaux Bègles a-t-elle été créée ?",
+    answers: ["1969", "1991", "2007", "2006"],
+    description:
+      "L'Union Bordeaux Bègles né de la fusion en 2006 du Stade bordelais université club et du Club athlétique béglais. Il est basé à Bègles dans la banlieue sud de Bordeaux en Gironde.",
+    correct: 3,
+  },
 
-  //   {
-  //     question: "Quelle est le nom de la vague bordelaise ?",
-  //     answers: ["Le mascaret", "Le mascara", "La mascarade", "Le masque à Ray"],
-  //     description:
-  //       "Le mascaret correspond à une brusque surélévation de l'eau d'un fleuve ou d'un estuaire provoqué par l'onde de la marée montante lors des grandes marées.Imperceptible la plupart du temps, il se manifeste au moment des nouvelles et pleines lunes. ",
-  //     correct: 0,
-  //   },
+  {
+    question: "Quelle est le nom de la vague bordelaise ?",
+    answers: ["Le mascaret", "Le mascara", "La mascarade", "Le masque à Ray"],
+    description:
+      "Le mascaret correspond à une brusque surélévation de l'eau d'un fleuve ou d'un estuaire provoqué par l'onde de la marée montante lors des grandes marées.Imperceptible la plupart du temps, il se manifeste au moment des nouvelles et pleines lunes. ",
+    correct: 0,
+  },
 ];
 
 let currentQuestionIndex = 0;
-let score = 6;
+let score = 0;
 let timerInterval;
 let timerTime = 20;
+let isSoundOn = true;
 audio.loop = true;
 
 soundButton.addEventListener("click", function () {
   if (audio.paused) {
     audio.play();
-    audio.volume = 0.5;
+    isSoundOn = true;
     imgAudio.src = "image/sound-on.png";
-    imgAudio.style.backgroundColor = "none";
   } else {
     audio.pause();
+    isSoundOn = false;
     imgAudio.src = "image/sound-off.png";
-    imgAudio.style.backgroundColor = "none";
   }
 });
 
@@ -231,16 +231,20 @@ function selectAnswer(answerIndex) {
     scoreDisplay.textContent = score;
     showFeedback("Bonne réponse !", "success");
     selectedAnswerElement.style.backgroundColor = "green";
-    const audioCorrect = document.createElement("audio");
-    audioCorrect.src = "./music/niceJob.m4a";
-    audioCorrect.play();
+
+    if (isSoundOn) {
+      const audioCorrect = new Audio("./music/niceJob.m4a");
+      audioCorrect.play();
+    }
   } else {
     showFeedback("Mauvaise réponse.", "error");
     selectedAnswerElement.style.backgroundColor = "red";
     correctAnswerElement.style.backgroundColor = "green";
-    const audioIncorrect = document.createElement("audio");
-    audioIncorrect.src = "./music/notToday.m4a";
-    audioIncorrect.play();
+
+    if (isSoundOn) {
+      const audioIncorrect = new Audio("./music/notToday.m4a");
+      audioIncorrect.play();
+    }
   }
 
   showDescription(questions[currentQuestionIndex].description);
@@ -254,7 +258,7 @@ function selectAnswer(answerIndex) {
       resetTimer();
     }, 8000);
   } else {
-    setTimeout(endQuiz, 1000);
+    setTimeout(endQuiz, 8000);
   }
 }
 
