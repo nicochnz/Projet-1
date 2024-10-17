d97809c51ce21369c3cf40a8ef0e56f2e3272b1a
nicochnz <chiche.nicolas@gmail.com>
2024-10-17 09:31:43 +0200

    new file for variables

---
+++ variables.js
@@ -0,0 +1,26 @@
+export const firstContainer = document.getElementById("firstContainer");
+export const questionContainer = document.getElementById("questionContainer");
+export const startButton = document.getElementById("startButton");
+export const questionText = document.getElementById("questionText");
+export const answerButtons = document.getElementById("answerButtons");
+export const recapContainer = document.getElementById("recapContainer");
+export const finalScoreText = document.getElementById("finalScore");
+export const restartButton = document.getElementById("restartButton");
+export const scoreDisplay = document.getElementById("score");
+export const timerSpan = document.getElementById("timer");
+
+export const descriptionQuestion = document.getElementById("description");
+export const h2RecapContainer = document.createElement("h2");
+
+export const input = document.createElement("input");
+export const numberQuestion = document.getElementById("numberQuestion");
+export const audio = document.getElementById("myAudio");
+export const soundButton = document.getElementById("sound-id");
+export const imgAudio = document.querySelector(".img-sound");
+export const imgRecap = document.createElement("img");
+export const classementBtn = document.createElement("button");
+export const classementContainer = document.createElement("section");
+export const difficultyBtn = document.getElementById("difficulty");
+export const easyBtn = document.getElementById("easy");
+export const hardBtn = document.getElementById("hard");
+export const videoBordeaux = document.querySelector("#videoBordeaux");
